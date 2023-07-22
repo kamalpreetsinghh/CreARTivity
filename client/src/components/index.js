e1d331f864ec162e0c610021b8f1b0e5c5a0fb89
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2023-07-21 21:45:14 -0400

    Show posts on Home Screen

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import FormField from "./FormField";
 import Loader from "./Loader";
+import Card from "./Card";
 
-export { Navbar, FormField, Loader };
+export { Navbar, FormField, Loader, Card };
